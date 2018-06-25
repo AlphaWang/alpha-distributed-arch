ef280bde56914647197404b56e699dd6931c6ede
Alpha <awang@coupang.com>
2018-06-25 22:02:50 +0800

    [distributed] init

---
+++ src/main/java/com/alphawang/distributed/DistributedApplication.java
@@ -0,0 +1,11 @@
+package com.alphawang.distributed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class DistributedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedApplication.class, args);
+    }
+}
