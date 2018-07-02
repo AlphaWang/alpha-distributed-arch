183bdceda08bc654f844d884893d0ac3b122facd
Alpha <awang@coupang.com>
2018-07-02 23:31:40 +0800

    [DIST] zookeeper connect

---
+++ src/main/java/com/alphawang/distributed/zookeeper/connect/ZKWatcher.java
@@ -0,0 +1,13 @@
+package com.alphawang.distributed.zookeeper.connect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+@Slf4j
+public class ZKWatcher implements Watcher {
+    @Override 
+    public void process(WatchedEvent event) {
+        log.info(">>> Watcher: accepted event {}", event);
+    }
+}
