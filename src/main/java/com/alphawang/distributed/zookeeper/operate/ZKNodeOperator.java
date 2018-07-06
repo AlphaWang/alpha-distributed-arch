1f8ea7c782cad6ebbd8ae4d8cd5c5359150f64d3
AlphaWang <1919wang@gmail.com>
2018-07-06 23:56:15 +0800

    [DIST] zookeeper operator: tune log

--- src/main/java/com/alphawang/distributed/zookeeper/operate/ZKNodeOperator.java
+++ src/main/java/com/alphawang/distributed/zookeeper/operate/ZKNodeOperator.java
@@ -55,6 +55,7 @@ public class ZkNodeOperator {
      */
     public void createZKNode(String path, byte[] data, List<ACL> acls) {
         try {
+            log.info("--- try to create node {}", path);
             String result = zooKeeper.create(path, data, acls, CreateMode.PERSISTENT);
             log.info("Created node: {}, result: {}", path, result);
 //            Thread.sleep(2000);
@@ -65,6 +66,7 @@ public class ZkNodeOperator {
     
     public void setZKNode(String path, byte[] data, int version) {
         try {
+            log.info("--- try to set node {} -> {}:{}", path, new String(data), version);
             Stat stat = zooKeeper.setData(path, data, version);
             log.info("Set node: {}; result: {}; version: {}", path, stat, stat.getVersion());
 //            Thread.sleep(2000);
@@ -75,6 +77,7 @@ public class ZkNodeOperator {
 
     public void setZKNodeAsync(String path, byte[] data, int version) {
 //        try {
+            log.info("--- try to async set node {} -> {}:{}", path, new String(data), version);
             zooKeeper.setData(path, data, version, new ZkStatCallback(), "Test Ctx");
             log.info("Set node async: {}.", path);
 //            Thread.sleep(2000);
@@ -85,6 +88,7 @@ public class ZkNodeOperator {
     
     public void deleteZKNode(String path, int version) {
         try {
+            log.info("--- try to delete node {} :{}", path, version);
             zooKeeper.delete(path, version);
             log.info("Delete node: {} {}", path, version);
 //            Thread.sleep(2000);
@@ -100,6 +104,7 @@ public class ZkNodeOperator {
      */
     public void deleteZKNodeAsync(String path, int version) {
 //        try {
+            log.info("--- try to async delete node {} :{}", path, version);
             zooKeeper.delete(path, version, new ZkVoidCallback(), "TEST Ctx delete");
             log.info("Delete node async: {} {}", path, version);
 //            Thread.sleep(2000);
@@ -107,22 +112,53 @@ public class ZkNodeOperator {
 //            log.error("Failed to delete node async: {}", path, e);
 //        }
     }
+    
+    public void readZkNode(String path) {
+        Stat stat = new Stat();
+        try {
+            byte[] data = zooKeeper.getData(path, false, stat);
+            log.info("==== get data {}: value={}, version={}", path, new String(data), stat.getVersion());
+        } catch (KeeperException | InterruptedException e) {
+            log.error("==== Failed to read data {}", path, e);
+        } 
+    }
 
     public static void main(String[] args) {
         ZkNodeOperator operator = new ZkNodeOperator(zkServerPath);
         
         operator.createZKNode("/testnode", "testnode-data".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE);
+        operator.readZkNode("/testnode");
+        
         operator.setZKNode("/testnode", "testnode-data-2".getBytes(), 0);
+        operator.readZkNode("/testnode");
+        
         operator.setZKNode("/testnode", "testnode-data-3".getBytes(), 0);
+        operator.readZkNode("/testnode");
+        
         operator.setZKNodeAsync("/testnode", "testnode-data-4".getBytes(), 1);
+        operator.readZkNode("/testnode");
 
+        
+        
         operator.createZKNode("/testdelete", "testnode-data".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE);
+        operator.readZkNode("/testdelete");
+        
         operator.deleteZKNode("/testdelete", 1);
+        operator.readZkNode("/testdelete");
+        
         operator.deleteZKNode("/testdelete", 0);
+        operator.readZkNode("/testdelete");
+        
+        
 
         operator.createZKNode("/testdelete2", "testnode-data2".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE);
+        operator.readZkNode("/testdelete2");
+        
         operator.deleteZKNodeAsync("/testdelete2", 1);
+        operator.readZkNode("/testdelete2");
+        
         operator.deleteZKNodeAsync("/testdelete2", 0);
+        operator.readZkNode("/testdelete2");
     }
     
 }
