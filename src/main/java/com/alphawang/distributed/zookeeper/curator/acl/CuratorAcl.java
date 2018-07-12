1d7b97b3702875901738137f496f4a84f3ceace2
AlphaWang <1919wang@gmail.com>
2018-07-12 22:38:32 +0800

    [DIST] zookeeper: org import

--- src/main/java/com/alphawang/distributed/zookeeper/curator/acl/CuratorAcl.java
+++ src/main/java/com/alphawang/distributed/zookeeper/curator/acl/CuratorAcl.java
@@ -8,7 +8,6 @@ import org.apache.zookeeper.data.ACL;
 import org.apache.zookeeper.data.Id;
 import org.apache.zookeeper.server.auth.DigestAuthenticationProvider;
 
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
