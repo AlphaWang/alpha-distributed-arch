e1a6e0117774508e33fb51e6197c395337d6709a
AlphaWang <1919wang@gmail.com>
2018-08-01 23:19:55 +0800

    [RPC] config : apache common configuration

---
+++ src/main/java/com/alphawang/distributed/config/apachecommon/ApacheCommonConfigurationDemo.java
@@ -0,0 +1,13 @@
+package com.alphawang.distributed.config.apachecommon;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.EnvironmentConfiguration;
+
+public class ApacheCommonConfigurationDemo {
+
+    public static void main(String[] args) {
+        Configuration configuration = new EnvironmentConfiguration();
+    }
+
+    
+}
